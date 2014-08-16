21a8398973ba0d61da6e5902989a6fd559369314
Eric Steinborn <esteinborn@gmail.com>
2014-08-15 20:44:36 -0400

    hopefully push this repo to dev repo

--- .travis-build.sh
+++ .travis-build.sh
@@ -8,16 +8,18 @@ if [ "$AUTHOR" != "Author: Travis-CI <travis@travis-ci.org>" -a "$TRAVIS_PULL_RE
 
   #copy data we're interested in to other place
   echo -e "Copying new files to a temp folder\n"
-  cp -R demo $HOME/demo
+  cp -R . $HOME/dev
 
   #go to home and setup git
   cd $HOME
-  #git config --global user.email "travis@travis-ci.org"
-  #git config --global user.name "Travis"
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
 
   #using token clone gh-pages branch
   echo -e "Cloning repo\n"
-  #git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/esteinborn/jquery-listnav.git  gh-pages > /dev/null
+  #git clone --quiet --branch=DEV https://${GH_TOKEN}@github.com/empirejustice/empirejustice.github.io.git  dev > /dev/null
+
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/empirejustice/dev.foreclosureguide.git  gh-pages > /dev/null
 
   #go into directory and copy data we're interested in to that directory
 
@@ -27,17 +29,17 @@ if [ "$AUTHOR" != "Author: Travis-CI <travis@travis-ci.org>" -a "$TRAVIS_PULL_RE
   echo -e "Deleting all existing repo content except for .git folder\n"
   # The next line came from Stack Overflow, love that site!
   # http://stackoverflow.com/a/22340057/682288
-  #find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
+  find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
 
   #echo -e "Copying new files into repo\n"
-  #cp -Rf $HOME/demo/* .
+  cp -Rf $HOME/dev/* .
 
   #add, commit and push files
-  #git add -f .
-  #git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  #git push -fq origin gh-pages > /dev/null
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
 
-  #echo -e "Done creating GitHub Pages branch!\n"
+  echo -e "Done creating GitHub Pages branch!\n"
 else
 
 echo -e "Skipping after script."
